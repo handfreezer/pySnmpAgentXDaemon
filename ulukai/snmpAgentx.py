504ed5298899cbe68d012314b351ed21796c16a7
Ether <laplagne@ulukai.net>
2022-05-06 00:21:03 +0200

    Adding tail plugin and update link to fork of pyagentx3

--- ulukai/snmpAgentx.py
+++ ulukai/snmpAgentx.py
@@ -20,6 +20,7 @@ import pyagentx3
 import re
 import psutil
 import os
+import time
 import threading
 import json
 
@@ -62,38 +63,19 @@ class snmpAgent(pyagentx3.Agent):
             logger.info("Agent : %s [plugin=%s on oid_ext=%s]", agentName, plugin, oid_ext)
             if "process" == plugin :
                 frequency = conf["frequency"]
-                logger.info("registering process plugin...")
+                logger.info("registering process plugin with frequency to %s ...", str(frequency))
                 self.register(self._oid_base + "." + oid_ext,
                     snmpAgentProcess,
                     freq=frequency,
                     data_store=conf["processes"])
                 logger.info(">registered")
-
-class snmpAgentProcess(pyagentx3.Updater):
-    def __init__(self, data_store=None):
-        pyagentx3.Updater.__init__(self)
-        self.conf = data_store
-
-    def update(self):
-        oids = dict()
-        for key in self.conf:
-            oids[key] = []
-        for proc in psutil.process_iter():
-            proc_dict = proc.as_dict()
-            for processKey,processConf in self.conf.items():
-                logger.info("%s : %s : %s - %s - %s - %r",
-                    self.__class__.__name__, processKey,
-                    processConf["description"], processConf["key"], processConf["regex"], processConf["list_pids"])
-                if re.search(processConf["regex"], proc_dict[processConf["key"]]):
-                    oids[processKey].append(proc_dict["pid"])
-        for key in oids:
-            pidCount = len(oids[key])
-            self.set_INTEGER(self.conf[key]["oid"] + ".0",
-                pidCount)
-            if True == self.conf[key]["list_pids"]:
-                for i in range(pidCount):
-                    self.set_INTEGER(self.conf[key]["oid"] + "." + str(i+1),
-                        oids[key][i])
+            if "tailStat" == plugin :
+                logger.info("registering tail plugin ...")
+                self.register(self._oid_base + "." + oid_ext,
+                    snmpAgentTail,
+                    freq=0,
+                    data_store=conf)
+                logger.info(">registered")
 
 class snmpAgentTail(pyagentx3.Updater):
     def __init__(self, data_store=None):
@@ -101,6 +83,8 @@ class snmpAgentTail(pyagentx3.Updater):
         self.conf = data_store
         self.filePath = self.conf["filePath"]
         self.regex = self.conf["regex"]
+        logger.debug("compiling regex [%s]", self.regex)
+        self.regexc = re.compile(self.regex)
 
     def run(self):
         fileFD = None
@@ -128,20 +112,28 @@ class snmpAgentTail(pyagentx3.Updater):
                         if os.stat(self.filePath).st_ino != fileIno:
                             fileFD.close()
                             fileFD = None
+                            logger.warning("tailed file inode changed")
                         else:
                             fileFD.seek(where)
                     except OSError:
                             fileFD.close()
                             fileFD = None
+                            logger.warning("tailed file disappearedi on OS FS")
                 else:
-                    match = re.search(self.regex, line)
-                    if match :
-                        self.last_result = match.groups()
-                        self.run_update()
+                    if None == line:
+                        logger.debug("no new line")
+                    else:
+                        logger.debug("new line [%s]", line)
+                        match = self.regexc.search(line)
+                        if match :
+                            self.last_result = match.groups()
+                            logger.debug("match result %r", self.last_result)
+                            self.run_update()
         if None != fileFD:
             fileFD.close()
             fileFD = None
-            fileIno = None     
+            fileIno = None
+            logger.info("tailed file closed on stopping thread")
 
     def update(self):
         i = 0
@@ -149,3 +141,52 @@ class snmpAgentTail(pyagentx3.Updater):
             i += 1
             self.set_OCTETSTRING(str(i), value)
         self.set_INTEGER("0", i)
+
+class snmpAgentProcess(pyagentx3.Updater):
+    def __init__(self, data_store=None):
+        pyagentx3.Updater.__init__(self)
+        self.conf = data_store
+        self.processKeyList = ['pid']
+        for key in self.conf:
+            self.conf[key]["regexc"] = re.compile(self.conf[key]["regex"])
+            if self.conf[key]["key"] not in self.processKeyList:
+                self.processKeyList.append(self.conf[key]["key"])
+
+    def update(self):
+        logger.info("start update of process plugin")
+        timeStart = time.time()
+        oids = dict()
+        for key in self.conf:
+            oids[key] = []
+        #timeInitOids = time.time()
+        for proc in psutil.process_iter(self.processKeyList):
+            #timeStartParsing = time.time()
+            #proc_dict = proc.as_dict()
+            proc_dict = proc.info
+            #logger.debug(" time parsing one process = %s - %r", time.time() - timeStartParsing, proc_dict)
+            for processKey,processConf in self.conf.items():
+                #logger.debug("%s : %s : %s - %s - %s - %r",
+                #    self.__class__.__name__, processKey,
+                #    processConf["description"], processConf["key"], processConf["regex"], processConf["list_pids"])
+                #if re.search(processConf["regex"], proc_dict[processConf["key"]]):
+                if None != proc_dict[processConf["key"]] and processConf["regexc"].search(proc_dict[processConf["key"]]):
+                    oids[processKey].append(proc_dict["pid"])
+        #timeParseProcess = time.time()
+        for key in oids:
+            pidCount = len(oids[key])
+            self.set_INTEGER(str(key) + ".0",
+                pidCount)
+            logger.info("adding %s[%s] with %s count", self.conf[key]["description"], key, pidCount)
+            if True == self.conf[key]["list_pids"]:
+                for i in range(pidCount):
+                    self.set_INTEGER(self.conf[key]["oid"] + "." + str(i+1),
+                        oids[key][i])
+        timeBuiltSnmp = time.time()
+        logger.debug("%s Stat : update in %ss",
+                self.__class__.__name__,timeBuiltSnmp - timeStart)
+        #logger.debug("%s Stat : oids %s - parsing %s - snmmp %s",
+        #        self.__class__.__name__,
+        #        timeInitOids - timeStart,
+        #        timeParseProcess - timeStart,
+        #        timeBuiltSnmp - timeStart)
+
